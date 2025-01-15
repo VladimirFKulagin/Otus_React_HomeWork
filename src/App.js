e7f748dbaabc6651bcc47db23590983128b6b8c2
VladimirFKulagin <kulaginvlad1991@gmail.com>
2025-01-15 18:53:42 +0300

    Start project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import RequestButtonPage from './pages/RequestButtonPage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ЗАпрос к API</h1>
+      <RequestButtonPage/>
+    </div>
+  );
+}
+
+export default App;
